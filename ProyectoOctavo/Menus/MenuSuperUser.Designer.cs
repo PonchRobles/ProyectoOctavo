b51acec146b42972b58b7d07f5fd8cbfcbdbb824
Alfonso Rodriguez <solo-metro@hotmail.com>
2017-03-21 16:31:42 -0600

    Se agrego menu para super usuario

---
+++ ProyectoOctavo/Menus/MenuSuperUser.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ProyectoOctavo.Menus
+{
+    partial class MenuSuperUser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPermisos = new System.Windows.Forms.Button();
+            this.txtTipoDeUsuario = new System.Windows.Forms.Button();
+            this.txtUsuarios = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPermisos
+            // 
+            this.txtPermisos.Location = new System.Drawing.Point(139, 101);
+            this.txtPermisos.Name = "txtPermisos";
+            this.txtPermisos.Size = new System.Drawing.Size(140, 47);
+            this.txtPermisos.TabIndex = 0;
+            this.txtPermisos.Text = "Permisos";
+            this.txtPermisos.UseVisualStyleBackColor = true;
+            this.txtPermisos.Click += new System.EventHandler(this.txtPermisos_Click);
+            // 
+            // txtTipoDeUsuario
+            // 
+            this.txtTipoDeUsuario.Location = new System.Drawing.Point(139, 205);
+            this.txtTipoDeUsuario.Name = "txtTipoDeUsuario";
+            this.txtTipoDeUsuario.Size = new System.Drawing.Size(140, 45);
+            this.txtTipoDeUsuario.TabIndex = 1;
+            this.txtTipoDeUsuario.Text = "Tipo de Usuario";
+            this.txtTipoDeUsuario.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuarios
+            // 
+            this.txtUsuarios.Location = new System.Drawing.Point(139, 154);
+            this.txtUsuarios.Name = "txtUsuarios";
+            this.txtUsuarios.Size = new System.Drawing.Size(140, 45);
+            this.txtUsuarios.TabIndex = 2;
+            this.txtUsuarios.Text = "Usuarios";
+            this.txtUsuarios.UseVisualStyleBackColor = true;
+            // 
+            // MenuSuperUser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 388);
+            this.Controls.Add(this.txtUsuarios);
+            this.Controls.Add(this.txtTipoDeUsuario);
+            this.Controls.Add(this.txtPermisos);
+            this.Name = "MenuSuperUser";
+            this.Text = "MenuSuperUser";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button txtPermisos;
+        private System.Windows.Forms.Button txtTipoDeUsuario;
+        private System.Windows.Forms.Button txtUsuarios;
+    }
+}
---
+++ ProyectoOctavo/Menus/MenuSuperUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoOctavo.Menus
+{
+    public partial class MenuSuperUser : Form
+    {
+        public MenuSuperUser()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPermisos_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
